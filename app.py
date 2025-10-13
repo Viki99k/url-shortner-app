37a768ce17bb5fd1d79a986725764f7bffbd28d5
vvk <vvk@DebianOS.myguest.virtualbox.org>
2025-10-13 16:49:34 +0000

    handling duplicate code name,flash and secret key in flask

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,flash
 import json
-
+import os.path
 
 app = Flask(__name__)
+app.secret_key = 'jn238722imeng3iut9089381dfij02nfnckkai'
 
 @app.route('/')
 def home():
@@ -12,9 +13,19 @@ def home():
 def your_url():
 	if request.method == 'POST':
 		urls = {}
+
+		if os.path.exists('urls.json'):
+			with open('urls.json') as urls_file:
+				urls = json.load(urls_file)
+
+		if request.form['code'] in urls.keys():
+			flash('The short name is already taken. Please select another name.')
+			return redirect(url_for('home'))
+
 		urls[request.form['code']] = {'url':request.form['url'] }
 		with open('urls.json','w') as url_file:
 			json.dump(urls, url_file)
 		return render_template('your_url.html',code=request.form['code'])
+
 	else:
 		return redirect(url_for('home'))
