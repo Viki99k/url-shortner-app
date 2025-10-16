f651d42ae41a9c5aa2b84445bd52d115a8c99b01
vvk <vvk@DebianOS.myguest.virtualbox.org>
2025-10-16 09:17:49 +0000

    working project mine with changes working course

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for,flash,abort
+from flask import Flask,render_template,request,redirect,url_for,flash,abort,session,jsonify
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -9,7 +9,7 @@ app.secret_key = 'jn238722imeng3iut9089381dfij02nfnckkai'
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('home.html',codes = session.keys())
 
 @app.route('/your-url',methods=['GET','POST'])
 def your_url():
@@ -35,6 +35,7 @@ def your_url():
 		
 		with open('urls.json','w') as url_file:
 			json.dump(urls, url_file)
+			session[request.form['code']]=True
 		return render_template('your_url.html',code=request.form['code'])
 
 	else:
@@ -57,3 +58,8 @@ def redirect_to_your_url(code):
 @app.errorhandler(404)
 def page_not_found(error):
 	return render_template('page_not_found.html'),404
+
+
+@app.route('/api')
+def session_api():
+	return jsonify(list(session.keys()))
